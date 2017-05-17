7304398c4385555e9dfa52451d2e086e223b1d2b
Mengqi7777 <memgqi@live.cn>
2017-05-17 09:23:22 +0800

    Initial commit

---
+++ src/main/java/com/poet/jettyserver/ConnMongoDB.java
@@ -0,0 +1,43 @@
+package com.poet.jettyserver;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by memgq on 2017/5/14.
+ */
+@Path("/")
+public class ConnMongoDB {
+
+
+        @GET
+        @Produces({"text/html"})
+        public String index(){
+            return "OK";
+        }
+
+        @GET
+        @Path("/mongodb")
+        @Produces(MediaType.TEXT_HTML)
+        public String mongodb(){
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("bbddb");
+            MongoCollection<Document> collection = mongoDatabase.getCollection("users");
+            Document document = new Document("title", "MongoDB");
+            List<Document> documents = new ArrayList<Document>();
+            documents.add(document);
+
+            collection.insertMany(documents);
+            return document.toString();
+        }
+
+
+
+}
